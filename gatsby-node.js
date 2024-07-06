1c3c7f8cf169f6fadc2e26c5855f6bd642dfc36d
Marcin Kotlicki <26150059+AyronK@users.noreply.github.com>
2024-07-06 16:58:09 +0000

    Added comment

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,8 @@
  * Why is it necessary?
  * https://www.gatsbyjs.com/docs/how-to/images-and-media/working-with-images-in-markdown/
  * https://github.com/danielmahon/gatsby-remark-relative-images/issues/61
+ * After the PR is merge (or a fork package is created) and package version upgrade on gatsby-remark-relative-images, this code is safe to be removed.
+ * https://github.com/danielmahon/gatsby-remark-relative-images/pull/64
  */
 function createNodeHandler(createNodeFieldCallable, node) {
   const eligibleToBeProcessed =
