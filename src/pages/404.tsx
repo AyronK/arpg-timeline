77c915158f1576573c529b1c3c3b123fbd417daf
Marcin Kotlicki <26150059+AyronK@users.noreply.github.com>
2024-07-06 14:04:32 +0000

    FIxes

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,8 +3,10 @@ import { Layout } from "../components/Layout";
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
+    <div className="container mx-auto px-4 mb-20">
+      <h1 className="text-4xl font-semibold text-center pt-12 pb-4">
+        <h1>NOT FOUND</h1>
+      </h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </div>
   </Layout>
