00c292f18de9a778d09bac5fc9a5780b797d536e
Lorddusk <lang2619@gmail.com>
2024-07-09 15:47:48 +0200

    Added a Time component that gives W/D/H/M/S a different color over the numbers. (#67)

---
+++ src/components/Time.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Time = ({
+  component,
+  char,
+}: {
+    component: string | number;
+    char: string;
+}) => (
+    <span>
+        {component}
+        <span className="text-emerald-500">{char}</span>
+    </span>
+);
