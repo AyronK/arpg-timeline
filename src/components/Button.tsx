b119e2231ea9b2ac92029ad34ac7e38f540d046a
AyronK <26150059+AyronK@users.noreply.github.com>
2024-07-21 00:41:36 +0200

    Added looking for mods

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,6 +12,7 @@ const buttonVariants = cva(
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+        warning: "bg-warning text-warning-foreground hover:bg-warning/90",
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
