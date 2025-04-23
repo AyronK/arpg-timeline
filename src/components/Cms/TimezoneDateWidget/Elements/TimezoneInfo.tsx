a2973d29adb055f3c471648bb3e78ee9085a618e
Marcin Kotlicki <26150059+AyronK@users.noreply.github.com>
2025-04-23 22:38:13 +0200

    Added CMS control to easily put dates in various timezones (#205)
    * Added timezone widged to decap
    
    * Refactor
    
    * F
    
    * lint

---
+++ src/components/Cms/TimezoneDateWidget/Elements/TimezoneInfo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { styles } from "@/components/Cms/TimezoneDateWidget/styles";
+
+export const TimezoneInfo: React.FC<{ label: string; value: string }> = ({
+  label,
+  value,
+}) => (
+  <div style={styles.info}>
+    <strong>{label}:</strong> {value}
+  </div>
+);
