b3f6b97172632108567c4cc87ea4d208b75dba5b
AyronK <kotlickimarcin@gmail.com>
2025-05-28 18:55:18 +0200

    Added discord button

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -20,7 +20,26 @@ export const Layout = ({ children }: React.PropsWithChildren) => {
                 <span className="text-nowrap">aRPG Timeline</span>
               </h1>
             </a>
-            <div className="relative right-0 flex flex-row items-center justify-center gap-2 md:absolute md:top-1/2 md:-translate-y-1/2">
+            <div className="relative right-0 flex flex-row items-center justify-center md:absolute md:top-1/2 md:-translate-y-1/2">
+              <Button variant={"ghost"} asChild className="px-2 2xl:px-4">
+                <a
+                  href="https://discord.gg/39mTbjkePg"
+                  rel="external noopener noreferrer"
+                  target="_blank"
+                  onPointerOver={() => sa_event("discord-hover")}
+                  onTouchStart={() => sa_event("discord-touch")}
+                  onClick={() => sa_event("discord-click")}
+                >
+                  <div className="grid h-[1.4rem] w-[1.4rem] place-content-center rounded-full bg-current 2xl:mr-3">
+                    <img
+                      src="/assets/discord-logo.svg"
+                      className="m-auto h-[1rem] w-[1rem]"
+                      alt="Buy me a coffee logo"
+                    />
+                  </div>
+                  <span className="hidden 2xl:block">Discord</span>
+                </a>
+              </Button>
               <Button variant={"ghost"} asChild className="px-2 2xl:px-4">
                 <a
                   href="https://www.buymeacoffee.com/ayron"
@@ -30,7 +49,7 @@ export const Layout = ({ children }: React.PropsWithChildren) => {
                   onTouchStart={() => sa_event("support-touch")}
                   onClick={() => sa_event("support-click")}
                 >
-                  <div className="grid h-[1.4rem] w-[1.4rem] place-content-center rounded-full bg-current 2xl:mr-2">
+                  <div className="grid h-[1.4rem] w-[1.4rem] place-content-center rounded-full bg-current 2xl:mr-3">
                     <img
                       src="/assets/bmc-logo-no-background.png"
                       className="m-auto h-[1rem] w-[1rem]"
@@ -40,20 +59,29 @@ export const Layout = ({ children }: React.PropsWithChildren) => {
                   <span className="hidden 2xl:block">Support me</span>
                 </a>
               </Button>
-              <Button variant={"ghost"} asChild className="px-2 2xl:px-4">
+              <Button
+                variant={"ghost"}
+                asChild
+                className="hidden px-2 sm:flex 2xl:px-4"
+              >
                 <a
                   href="https://github.com/AyronK/arpg-timeline/issues"
                   rel="external nofollow noreferrer"
                   target="_blank"
+                  onPointerOver={() => sa_event("github-hover")}
+                  onTouchStart={() => sa_event("github-touch")}
+                  onClick={() => sa_event("github-click")}
                 >
-                  <svg
-                    aria-hidden="true"
-                    viewBox="0 0 16 16"
-                    version="1.1"
-                    className="h-[1.2rem] w-[1.2rem] fill-current 2xl:mr-2"
-                  >
-                    <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
-                  </svg>
+                  <div className="grid h-[1.4rem] w-[1.4rem] place-content-center rounded-full bg-current 2xl:mr-3">
+                    <svg
+                      aria-hidden="true"
+                      viewBox="0 0 16 16"
+                      version="1.1"
+                      className="h-[1.2rem] w-[1.2rem]"
+                    >
+                      <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+                    </svg>
+                  </div>
                   <span className="hidden 2xl:block">Request a feature</span>
                 </a>
               </Button>
