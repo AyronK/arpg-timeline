cbdb7c2680aec50111eecd3d787986956fe373de
AyronK <kotlickimarcin@gmail.com>
2025-06-04 22:37:27 +0200

    Fixed CLS

--- arpg-timeline/src/components/Home/Events.tsx
+++ arpg-timeline/src/components/Home/Events.tsx
@@ -14,6 +14,7 @@ const Timeline = lazy(() =>
 
 export const Events = ({ events }: { events: TimelineEvent[] }) => (
     <div className="bg-card text-card-foreground 3xl:col-span-4 4xl:col-span-5 lg-col-span-2 relative order-3 col-span-1 flex flex-col gap-4 rounded-md border p-4 md:col-span-2 md:gap-4 md:p-6 xl:col-span-3">
+        <h3 className="mb-1.5 text-xs">Timeline</h3>
         <Suspense
             fallback={
                 <div className="h-[255px] md:h-[296px]">
@@ -21,7 +22,6 @@ export const Events = ({ events }: { events: TimelineEvent[] }) => (
                 </div>
             }
         >
-            <h3 className="mb-1.5 text-xs">Timeline</h3>
             <ErrorBoundary fallback={<WidgetDiedFallback />}>
                 <Timeline events={events} />
             </ErrorBoundary>
