54b0951ab0ee10757961ce9cf98591f19a016f94
AyronK <kotlickimarcin@gmail.com>
2025-06-04 20:11:46 +0200

    F

--- arpg-timeline/next.config.ts
+++ arpg-timeline/next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    experimental: {
-        ppr: "incremental",
-    },
+    // TODO check
+    // experimental: {
+    //     ppr: "incremental",
+    // },
 };
 
 export default nextConfig;
