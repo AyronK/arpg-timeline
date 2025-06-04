f2c1241dca113fa90e8d18fdb0de9c7835715e9a
AyronK <kotlickimarcin@gmail.com>
2025-06-04 21:50:59 +0200

    F

--- arpg-timeline/src/components/Home/HomePage.tsx
+++ arpg-timeline/src/components/Home/HomePage.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from "react";
+
 import { Game, GameStream } from "@/lib/cms/games.types";
 
 import { GamesAndEventsGrid } from "./GamesAndEventsGrid";
@@ -5,11 +7,13 @@ import { Streams } from "./Streams";
 
 export const Main = ({ games, streams }: { games: Game[]; streams: GameStream[] }) => {
     return (
-        <div className="mt-4 flex flex-col gap-4 xl:mt-8">
-            <Streams games={games} streams={streams} />
-            <div className="relative flex flex-col gap-4">
-                <GamesAndEventsGrid games={games} />
+        <Suspense>
+            <div className="mt-4 flex flex-col gap-4 xl:mt-8">
+                <Streams games={games} streams={streams} />
+                <div className="relative flex flex-col gap-4">
+                    <GamesAndEventsGrid games={games} />
+                </div>
             </div>
-        </div>
+        </Suspense>
     );
 };
