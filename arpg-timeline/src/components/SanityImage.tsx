1826fd8c49f90283ccfc3a6ed5dac924d2ed8fbe
AyronK <kotlickimarcin@gmail.com>
2025-06-08 17:30:41 +0200

    F

--- arpg-timeline/src/components/SanityImage.tsx
+++ arpg-timeline/src/components/SanityImage.tsx
@@ -1,35 +1,16 @@
 import createImageUrlBuilder from "@sanity/image-url";
 import { ImageProps } from "next/image";
 import Image from "next/image";
+import { SanityImageAssetDocument } from "next-sanity";
 
 import { dataset, projectId } from "@/lib/sanity.api";
-
-type ImageType = {
-    url: string | undefined;
-    alternativeText?: string | undefined;
-};
-type SanityImageBlock = SanityBase<{
-    _type?: "imageBlock";
-    alternativeText?: string;
-    asset?: SanityImageBlockAsset;
-    lqip?: string;
-}>;
-type SanityImageBlockAsset = {
-    _ref: string;
-    _type: "reference";
-};
-type SanityBase<T = unknown> = T & {
-    _key?: string | undefined;
-    _type?: string | undefined;
-};
 
 const imageBuilder = createImageUrlBuilder({
     projectId: projectId || "",
     dataset: dataset || "",
 });
 
-export const urlForImage = (source: Image) => {
-    // Ensure that source image contains a valid reference
+export const urlForImage = (source: SanityImageAssetDocument) => {
     if (!source?.asset?._ref) {
         return undefined;
     }
@@ -41,13 +22,14 @@ export const SanityImage = ({
     src,
     objectFit = "cover",
     ...remaining
-}: ImageProps & { objectFit: "cover" | "contain" }) => {
-    const imageUrl = (src as ImageType)?.url ?? urlForImage(src as SanityImageBlock)?.url();
+}: ImageProps & { src: SanityImageAssetDocument } & { objectFit: "cover" | "contain" }) => {
+    if (!src) {
+        return null;
+    }
 
-    const altText =
-        (src as SanityImageBlock)?.alternativeText ?? (src as ImageType)?.alternativeText;
-
-    const imageLqip = (src as SanityImageBlock)?.lqip;
+    const imageUrl = src.url ?? urlForImage(src)?.url();
+    const altText = src.alternativeText;
+    const imageLqip = src.lqip;
 
     if (!imageUrl) {
         return null;
