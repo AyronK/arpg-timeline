61ae3ff4f82aeda775914d4242e357bbe8deb04a
AyronK <kotlickimarcin@gmail.com>
2025-07-18 16:49:46 +0200

    F

--- arpg-timeline/src/hoc/GameToSeasonWidget/types.ts
+++ arpg-timeline/src/hoc/GameToSeasonWidget/types.ts
@@ -1,7 +1,5 @@
 "use client";
 
-import { Selector } from "./types";
-
 export type Selector = "current" | "next";
 export const SelectorLabels: Record<Selector, string> = {
     current: "Current",
