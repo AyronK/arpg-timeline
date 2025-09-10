db0f9705aaa2068db7020792136090e4a66006ff
Marcin Kotlicki <26150059+AyronK@users.noreply.github.com>
2025-09-10 19:20:07 +0200

    Update getStructuredDataForGame.ts

--- arpg-timeline/src/lib/games/getStructuredDataForGame.ts
+++ arpg-timeline/src/lib/games/getStructuredDataForGame.ts
@@ -1,54 +1,53 @@
 import { Game } from "@/lib/cms/games.types";
 
 export const getStructuredDataForGame = (game: Game) => {
-    if (!game || !game.currentSeason || !game.nextSeason) return null;
-
+    if (!game) return null;
+    
     const structuredData = [];
-
+    
     if (
         game.currentSeason &&
-        !!game.currentSeason.start?.confirmed &&
-        !!game.currentSeason.start?.startDate &&
-        !!game.currentSeason.end?.endDate
+        game.currentSeason.start?.confirmed &&
+        game.currentSeason.start?.startDate &&
+        game.currentSeason.end?.endDate
     ) {
         structuredData.push({
+            "@context": "https://schema.org",
             "@type": "Event",
             name: game.currentSeason.name || "Current Season",
-            startDate: game.currentSeason.start?.confirmed
-                ? game.currentSeason.start?.startDate || null
-                : null,
-            endDate: game.currentSeason.end?.confirmed
-                ? game.currentSeason.end?.endDate || null
-                : null,
-            eventStatus: game.currentSeason.start?.confirmed
-                ? "https://schema.org/EventScheduled"
-                : null,
+            startDate: game.currentSeason.start.startDate,
+            endDate: game.currentSeason.end.endDate,
+            eventStatus: "https://schema.org/EventScheduled",
             eventAttendanceMode: "https://schema.org/OnlineEventAttendanceMode",
-            location: { "@type": "VirtualLocation", url: game.url || null },
+            location: {
+                "@type": "VirtualLocation",
+                name: game.name,
+                url: game.url
+            }
         });
     }
-
+    
     if (
         game.nextSeason &&
-        !!game.nextSeason.start?.confirmed &&
-        !!game.nextSeason.start?.startDate &&
-        !!game.nextSeason.end?.endDate
+        game.nextSeason.start?.confirmed &&
+        game.nextSeason.start?.startDate &&
+        game.nextSeason.end?.endDate
     ) {
         structuredData.push({
+            "@context": "https://schema.org",
             "@type": "Event",
             name: game.nextSeason.name || "Next Season",
-            startDate: game.nextSeason.start?.confirmed
-                ? game.nextSeason.start?.startDate || null
-                : null,
-            endDate: game.nextSeason.end?.confirmed ? game.nextSeason.end?.endDate || null : null,
-            eventStatus: game.nextSeason.start?.confirmed
-                ? "https://schema.org/EventScheduled"
-                : null,
+            startDate: game.nextSeason.start.startDate,
+            endDate: game.nextSeason.end.endDate,
+            eventStatus: "https://schema.org/EventScheduled",
             eventAttendanceMode: "https://schema.org/OnlineEventAttendanceMode",
-            location: { "@type": "VirtualLocation", url: game.url || null },
+            location: {
+                "@type": "VirtualLocation",
+                name: game.name,
+                url: game.url
+            }
         });
     }
-
+    
     return structuredData.length > 0 ? structuredData : null;
 };
-
